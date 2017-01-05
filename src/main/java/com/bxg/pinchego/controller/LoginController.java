59d0230008c40ad6cc44e844dd05a694d87ec07e
gaobin <gaobin@workway.com.cn>
2017-01-05 17:14:23 +0800

    页面跳转

---
+++ src/main/java/com/bxg/pinchego/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.bxg.pinchego.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author gaobin
+ * @createDate ${Date}
+ */
+@RequestMapping("/login")
+public class LoginController {
+
+}
--- src/main/java/com/bxg/pinchego/controller/MainController.java
+++ src/main/java/com/bxg/pinchego/controller/MainController.java
@@ -1,7 +1,10 @@
 package com.bxg.pinchego.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author gaobin
@@ -12,5 +15,17 @@ public class MainController {
     @RequestMapping("/")
     public String index(){
         return "car";
+    }
+    @RequestMapping("/{pagename}")
+    public String ticket(@PathVariable String pagename){
+        return "html/"+pagename;
+    }
+    @RequestMapping("/loginIn")
+    public String login(HttpServletRequest request){
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+       return "";
+
     }
 }
