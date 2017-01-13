1b0b938c697e48ead6f83a273104485999bdd81f
gaobin <gaobin@workway.com.cn>
2017-01-13 08:35:26 +0800

    拼车信息添加功能,和数据展示

---
+++ src/main/java/com/bxg/pinchego/repository/CarpoolInfoRepository.java
@@ -0,0 +1,12 @@
+package com.bxg.pinchego.repository;
+
+import com.bxg.pinchego.model.CarpoolInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author gaobin
+ * @createDate ${Date}
+ */
+public interface CarpoolInfoRepository extends JpaRepository<CarpoolInfo, Integer> {
+
+}
